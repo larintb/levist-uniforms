53fd4907be05aed7593ccd29cb385b68297e09fa
larintb <vlpizana@gmail.com>
2025-06-25 18:17:44 -0500

    Size Added

--- components/admin/ProductForm.tsx
+++ components/admin/ProductForm.tsx
@@ -52,7 +52,7 @@ export interface ProductFormProps {
   initialData?: InitialProductData;
 }
 
-const SIZES = ["XXS","XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL", "5XL"];
+const SIZES = ["XXS","XS", "S", "M", "L", "XL", "1X", "2XL", "3XL", "4XL", "5XL"];
 
 // --- Iconos ---
 const PlusIcon = (props: React.SVGProps<SVGSVGElement>) => <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" /></svg>;
